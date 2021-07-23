5dc8ed9b2a585d584751aaecd36a761ab34a9662
Jefferson Jacome <jeffesjacome@gmail.com>
2021-07-23 17:15:56 -0500

    add filter table

--- src/app/ibox/ibox.component.ts
+++ src/app/ibox/ibox.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { Product } from '../product';
 import { ProductService } from '../productservice';
 import { ActivatedRoute } from '@angular/router';
+import { SelectItem } from 'primeng/api';
 @Component({
   selector: 'app-ibox',
   templateUrl: './ibox.component.html',
@@ -10,76 +11,91 @@ import { ActivatedRoute } from '@angular/router';
 export class IboxComponent implements OnInit {
   id: string = '';
   productDialog: boolean = false;
-
+  editing: boolean = false;
   products: Product[] = [];
-
+  rowGroupMetadata: any;
+  carname: string = 'asdasd';
   product: Product = {};
+  products1: Product[] = [];
+  products2: Product[] = [];
 
+  statuses: SelectItem[] = [];
+
+  clonedProducts: { [s: string]: Product; } = {};
   selectedProducts: Product[] = [];
 
   submitted: boolean = false;
 
-  constructor(private AR: ActivatedRoute, private productService: ProductService,) { }
+  constructor(private AR: ActivatedRoute, private productService: ProductService,) {
+    this.product = {
+      "id": "1000",
+      "name": "James Butt",
+      "country": {
+        "name": "Algeria",
+        "code": "dz"
+      },
+      "company": "Benton, John B Jr",
+      "date": "2015-09-13",
+      "status": "unqualified",
+      "activity": 17,
+      "representative": {
+        "name": "Ioni Bowcher",
+        "image": "ionibowcher.png"
+      }
+
+    }
+  }
 
   ngOnInit() {
     this.id = this.AR.snapshot.paramMap.get('id')!
     this.productService.getProducts().then(data => this.products = data);
   }
-
-  openNew() {
-    this.product = {};
-    this.submitted = false;
-    this.productDialog = true;
+  onSort() {
+    this.updateRowGroupMetaData();
   }
 
-  editProduct(product: Product) {
-    this.product = { ...product };
-    this.productDialog = true;
-  }
+  updateRowGroupMetaData() {
+    this.rowGroupMetadata = {};
 
-  hideDialog() {
-    this.productDialog = false;
-    this.submitted = false;
-  }
+    if (this.products !== null) {
+      for (let i = 0; i < this.products.length; i++) {
+        let rowData = this.products[i];
+        let representativeName = rowData.representative!.name;
 
-  saveProduct() {
-    this.submitted = true;
-    if (this.product.name !== undefined) {
-      if (this.product.name.trim()) {
-        if (this.product.id) {
-          this.products[this.findIndexById(this.product.id)] = this.product;
+        if (i == 0) {
+          this.rowGroupMetadata[representativeName!] = { index: 0, size: 1 };
         }
         else {
-          this.product.id = this.createId();
-          this.product.image = 'product-placeholder.svg';
-          this.products.push(this.product);
+          let previousRowData = this.products[i - 1];
+          let previousRowGroup = previousRowData.representative!.name;
+          if (representativeName === previousRowGroup)
+            this.rowGroupMetadata[representativeName!].size++;
+          else
+            this.rowGroupMetadata[representativeName!] = { index: i, size: 1 };
         }
-
-        this.products = [...this.products];
-        this.productDialog = false;
-        this.product = {};
       }
     }
   }
 
-  findIndexById(id: string): number {
-    let index = -1;
-    for (let i = 0; i < this.products.length; i++) {
-      if (this.products[i].id === id) {
-        index = i;
-        break;
-      }
-    }
-
-    return index;
+  onRowEditInit(product: Product) {
+    this.clonedProducts[product.id!] = { ...product };
+    this.editing = !this.editing;
   }
 
-  createId(): string {
-    let id = '';
-    var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    for (var i = 0; i < 5; i++) {
-      id += chars.charAt(Math.floor(Math.random() * chars.length));
+  onRowEditSave(product: Product) {
+
+    if (product.price! > 0) {
+      delete this.clonedProducts[product.id!];
+
     }
-    return id;
+    this.editing = !this.editing;
+
   }
+
+  onRowEditCancel(product: Product, index: number) {
+    this.products[index] = this.clonedProducts[product.id!];
+    delete this.clonedProducts[product.id!];
+    this.editing = !this.editing;
+  }
+
 }
